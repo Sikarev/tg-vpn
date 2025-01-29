25bff1e91ed79fb0b49594b25dbc1f0b4fedc5a5
Sikarev <sikarev.roman@yandex.ru>
2025-01-29 22:49:28 +0300

    router + styles

--- src/pages/info-page.tsx
+++ src/pages/info-page.tsx
@@ -1,7 +1,17 @@
 import { FC } from 'react';
+import { Card } from 'antd-mobile';
 
 export const InfoPage: FC = () => {
   return (
-    <div>info page</div>
+    <div className="flex flex-col items-center">
+      <div className="font-bold text-3xl my-20">Best VPN</div>
+
+      <div className="flex flex-col gap-2">
+        <Card>
+          Интернет летает
+          Благодаря нашим лучшим протоколам подключения
+        </Card>
+      </div>
+    </div>
   );
 }
